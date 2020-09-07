dd7bebf0908c678d9dfdacecb9df9a27dc72c5ba
Shen Liao <shen.liao@thoughtworks.com>
2020-09-07 19:11:21 +0800

    [Shen] feat: add crossorigin annotation

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentListAPI.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/api/StudentListAPI.java
@@ -2,11 +2,13 @@ package com.thoughtworks.capability.gtb.entrancequiz.api;
 
 import com.thoughtworks.capability.gtb.entrancequiz.Service.StudentListService;
 import com.thoughtworks.capability.gtb.entrancequiz.domain.Student;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class StudentListAPI {
     private final StudentListService studentListService;
