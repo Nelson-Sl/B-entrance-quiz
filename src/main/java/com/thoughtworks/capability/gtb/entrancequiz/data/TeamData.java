a1c50a59bfa1417c9e0a3b72eb4fd2811ae022e6
Shen Liao <shen.liao@thoughtworks.com>
2020-09-07 21:19:06 +0800

    [Shen] feat: refactor the algorithm of getting team members

--- src/main/java/com/thoughtworks/capability/gtb/entrancequiz/data/TeamData.java
+++ src/main/java/com/thoughtworks/capability/gtb/entrancequiz/data/TeamData.java
@@ -5,13 +5,13 @@ import com.thoughtworks.capability.gtb.entrancequiz.domain.Team;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class TeamData {
     private static final String DEFAULT_TEAM_NAME_PREFIX = "Team ";
-    private static List<Student> studentList = StudentData.getStudentList();
-    private static List<Integer> usedNum = new ArrayList<>();
+    private static List<Student> shuffleStudentList =  new ArrayList<>();
     private static Team[] teamList = new Team[] {
             new Team(DEFAULT_TEAM_NAME_PREFIX + 1, new ArrayList<Student>()),
             new Team(DEFAULT_TEAM_NAME_PREFIX + 2, new ArrayList<Student>()),
@@ -21,36 +21,30 @@ public class TeamData {
             new Team(DEFAULT_TEAM_NAME_PREFIX + 6, new ArrayList<Student>()),
     };
 
-    public static Team[] getRandomTeams() {
-        int randomRound = (int) Math.ceil((double) studentList.size() / teamList.length);
+    public TeamData() {
+        List<Student> currentStudentList = StudentData.getStudentList();
+        for(Student student: currentStudentList) {
+            shuffleStudentList.add(student);
+        }
+    }
 
-        //清空usedNum
-        usedNum.clear();
+    public static Team[] getRandomTeams() {
+        int randomRound = (int) Math.ceil((double) shuffleStudentList.size() / teamList.length);
+
+        //打乱排序
+        Collections.shuffle(shuffleStudentList);
 
         //第一轮清空team的学生，然后逐次添加
         for(int round = 0; round < randomRound; round++) {
-            int teamChosenTime = round == randomRound - 1 ? studentList.size() % teamList.length : teamList.length;
+            int teamChosenTime = round == randomRound - 1 ? shuffleStudentList.size() % teamList.length : teamList.length;
             for(int team = 0; team < teamChosenTime; team++) {
                 if(round == 0) {
                     teamList[team].setTeamMembers(new ArrayList<Student>());
                 }
-                chooseRandomStudent(teamList[team]);
+                teamList[team].getTeamMembers().add(shuffleStudentList.get(round * teamList.length + team));
             }
         }
 
         return teamList;
-    }
-
-    private static void chooseRandomStudent(Team team) {
-        int randomNumber = (int) ((Math.random()*34)+1);
-        if(!usedNum.contains(randomNumber)) {
-            for(Student student: studentList) {
-                if(student.getSid() == randomNumber) {
-                    team.getTeamMembers().add(student);
-                    return ;
-                }
-            }
-        }
-        chooseRandomStudent(team);
     }
 }
